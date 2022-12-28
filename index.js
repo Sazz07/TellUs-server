83e3425c852bff41f80234fa349145ab51bbb6f2
Sazz07 <sazzad.hossain882@gmail.com>
2022-12-29 02:12:54 +0600

    Mongodb connection done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
@@ -12,7 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 
-
+//MongoDb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.teba24n.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 
